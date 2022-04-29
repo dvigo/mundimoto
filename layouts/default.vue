1000105fb8fe10b5acfd52115f0631f7e37586ea
David Vigo <dvigodev@Davids-MacBook-Air.local>
2022-04-29 23:40:44 +0200

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'default',
+  data () {
+    return {
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      title: 'MundiMoto'
+    }
+  }
+}
+</script>
