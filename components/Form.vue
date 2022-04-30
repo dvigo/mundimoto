f935a6ea96ceba25e250408810a46b361ca24e76
Josep Maria Salvia Hornos <josh.salvia@gmail.com>
2022-04-30 23:02:30 +0200

    basic recommendation system

--- components/Form.vue
+++ components/Form.vue
@@ -1,23 +1,26 @@
 <template lang="pug">
-  v-card
-    v-card-title Recommendation BY Artificial Intelligence
-      v-form
-        v-text-field(type="number" label="Design year")
-        v-text-field(type="number" label="CC")
-        v-text-field(type="number" label="Top Speed (Km/h)" ) // Top Speed KM/h
-        v-text-field(type="number" label="Power (kW)")
-        v-text-field(type="number" label="Full capacity")
-        v-text-field(type="number" label="Weight (Kg)")
-        v-text-field(type="number" label="Valves per cylinder")
-        v-select(
-          :items="categories"
-          label="Category"
-          v-model="selected"
-          @change="changeData")
-
+v-card
+  v-card-title Recommendation BY Artificial Intelligence
+    <form @submit.prevent="submitForm">
+      v-text-field(type="number", name="model_year", label="Design year")
+      v-text-field(type="number", name="displacement", label="CC")
+      v-text-field(type="number", label="Top Speed (Km/h)")
+      v-text-field(type="number", label="Power (kW)")
+      v-text-field(type="number", label="Full capacity")
+      v-text-field(type="number", label="Weight (Kg)")
+      v-text-field(type="number", label="Valves per cylinder")
+      v-select(
+        :items="categories",
+        label="Category",
+        v-model="selected",
+      )
+      <v-btn type="submit" color="success">Submit</v-btn>
+    </form>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: "Form",
   data() {
@@ -38,19 +41,30 @@ export default {
         "Sport",
         "Sport touring",
         "Speedway",
-        "Prototype / concept model"
+        "Prototype / concept model",
       ],
-      selected: ''
-    }
+      selected: "",
+    };
   },
   methods: {
-    changeData() {
-      console.log(this.selected);
-    }
-  }
-}
+    submitForm() {
+      console.log('eoo');
+      console.log(this.form.model_year);
+      this.$axios.$get("/variant/recommendation", this.form)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((error) => {
+          console.log(error);
+          // error.response.status Check status code
+        })
+        .finally(() => {
+          //Perform action in always
+        });
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
